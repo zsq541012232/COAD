738e007c41bdce51e35fbb77ec17cb4ddb39a024
siqizhou <541012232@qq.com>
2022-08-06 02:30:06 +0800

    111

--- script/plans/plan.py
+++ script/plans/plan.py
@@ -7,6 +7,11 @@ optimizers = ['BBO','PSO','SMA','AEO','HS']
 instances = [
     ['data/dataset1/instance1','2022-03-20 00:00:00','instance1'],
     ['data/dataset1/instance2', '2022-03-21 00:00:00','instance2'],
+    ['data/dataset1/instance3', '2022-03-20 00:00:00', 'instance3'],
+    ['data/dataset1/instance4', '2022-03-21 00:00:00', 'instance4'],
+    ['data/dataset1/instance5', '2022-03-20 00:00:00', 'instance5'],
+    ['data/dataset1/instance6', '2022-03-21 00:00:00', 'instance6'],
+    ['data/dataset1/instance7', '2022-03-24 00:00:00', 'instance7'],
 ]
 
 
