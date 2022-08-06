ffb5fb74055cce6d07cb381df7e4b1065ce4a308
siqizhou <541012232@qq.com>
2022-08-06 14:42:28 +0800

    111

--- script/plans/plan.py
+++ script/plans/plan.py
@@ -5,13 +5,19 @@ model_names = ['GMM']
 optimizers = ['BBO','PSO','SMA','AEO','HS']
 
 instances = [
-    ['data/dataset1/instance1','2022-03-20 00:00:00','instance1'],
-    ['data/dataset1/instance2', '2022-03-21 00:00:00','instance2'],
-    ['data/dataset1/instance3', '2022-03-20 00:00:00', 'instance3'],
-    ['data/dataset1/instance4', '2022-03-21 00:00:00', 'instance4'],
-    ['data/dataset1/instance5', '2022-03-20 00:00:00', 'instance5'],
-    ['data/dataset1/instance6', '2022-03-21 00:00:00', 'instance6'],
-    ['data/dataset1/instance7', '2022-03-24 00:00:00', 'instance7'],
+    ['data/dataset1/instance1','2022-03-20 00:00:00','dataset1_instance1'],
+    ['data/dataset1/instance2', '2022-03-21 00:00:00','dataset1_instance2'],
+    ['data/dataset1/instance3', '2022-03-20 00:00:00', 'dataset1_instance3'],
+    ['data/dataset1/instance4', '2022-03-21 00:00:00', 'dataset1_instance4'],
+    ['data/dataset1/instance5', '2022-03-20 00:00:00', 'dataset1_instance5'],
+    ['data/dataset1/instance6', '2022-03-21 00:00:00', 'dataset1_instance6'],
+    ['data/dataset1/instance7', '2022-03-24 00:00:00', 'dataset1_instance7'],
+    ['data/dataset2/instance1', '2022-05-01 00:00:00', 'dataset2_instance1'],
+    ['data/dataset2/instance2', '2022-05-03 00:00:00', 'dataset2_instance2'],
+    ['data/dataset2/instance3', '2022-05-05 00:00:00', 'dataset2_instance3'],
+    ['data/dataset2/instance4', '2022-05-07 00:00:00', 'dataset2_instance4'],
+    ['data/dataset2/instance5', '2022-05-09 00:00:00', 'dataset2_instance5'],
+
 ]
 
 
