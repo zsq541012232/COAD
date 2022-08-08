2cb7fb5fc311e25b49081e9f76f8940500a00dbf
siqizhou <541012232@qq.com>
2022-08-08 12:58:35 +0800

    111

--- script/analyze/run.py
+++ script/analyze/run.py
@@ -1,5 +1,5 @@
 from functions.utils.filetools import read_ground_truth_file, read_list
-from functions.utils.plottools import plot_threshold_pre_recall, plot_prc_curve
+from functions.utils.plottools import plot_threshold_pre_recall, plot_prc_curve, plot_roc_curve
 from script.detection.run import get_instance
 
 
@@ -28,6 +28,14 @@ def get_score_path():
     path = input('Score path?')
     return path
 
+def roc(score_path, instance_):
+    test_time_base = instance_[1]
+    ground_truth_path = '../../' + instance_[0] + '/ground_truth.csv'
+
+    anomaly_time_point_ground_truth, l, c, f = read_ground_truth_file(test_time_base, ground_truth_path)
+    anomaly_scores_at_time = read_list(score_path)
+    plot_roc_curve(anomaly_time_point_ground_truth, anomaly_scores_at_time, 'roc')
+
 
 if __name__ == '__main__':
     result_score_path = get_score_path()
@@ -36,3 +44,5 @@ if __name__ == '__main__':
     plot_pre_recall_by_threshold(result_score_path, instance)
 
     plot_prc(result_score_path, instance)
+
+    roc(result_score_path, instance)
