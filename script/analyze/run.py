9970ccbbc8b3c7351024d535162b26451488b6fd
siqizhou <541012232@qq.com>
2022-09-14 16:44:52 +0800

    111

--- script/analyze/run.py
+++ script/analyze/run.py
@@ -1,7 +1,8 @@
 from functions.utils.filetools import read_ground_truth_file, read_list
-from functions.utils.judgement import get_pre_recall_f1
+from functions.utils.judgement import get_pre_recall_f1, get_current_pre_and_recall
 from functions.utils.plottools import plot_threshold_pre_recall, plot_prc_curve, plot_roc_curve
 from script.detection.run import get_instance, get_type, get_model, get_optimizer
+import random
 
 
 def plot_pre_recall_by_threshold(score_path, instance_, method_name):
@@ -70,6 +71,42 @@ def get_method_name():
     return return_method_name
 
 
+def get_f1(anomaly_time_point_ground_truth, anomaly_scores_at_time, percent):
+    max_score = max(anomaly_scores_at_time)
+    min_score = min(anomaly_scores_at_time)
+    span_score = max_score - min_score
+    threshold = max_score - percent * span_score
+    pre, recall = get_current_pre_and_recall(anomaly_time_point_ground_truth,
+                                             anomaly_scores_at_time,
+                                             threshold)
+    if pre + recall == 0:
+        f1 = 0
+    else:
+        f1 = 2 * pre * recall / (pre + recall)
+
+    return f1
+
+
+def putout_f1_by_random_choose_threshold(score_path, instance_):
+    f1_list = []
+    random_choose_time = 10000
+
+    test_time_base = instance_[1]
+    ground_truth_path = '../../' + instance_[0] + '/ground_truth.csv'
+
+    anomaly_time_point_ground_truth, l, c, f = read_ground_truth_file(test_time_base, ground_truth_path)
+    anomaly_scores_at_time = read_list(score_path)
+
+    for i in range(random_choose_time):
+        random_percent = random.random()
+        f1 = get_f1(anomaly_time_point_ground_truth=anomaly_time_point_ground_truth,
+                    anomaly_scores_at_time=anomaly_scores_at_time,
+                    percent=random_percent)
+        f1_list.append(f1)
+
+    return sum(f1_list) / len(f1_list)
+
+
 if __name__ == '__main__':
     result_score_path = get_score_path()
     instance = get_instance()
@@ -82,3 +119,6 @@ if __name__ == '__main__':
     plot_roc(result_score_path, instance, method_name)
 
     putout_f1_by_threshold_percent(result_score_path, instance)
+
+    avg_f1 = putout_f1_by_random_choose_threshold(result_score_path, instance)
+    print('avg_f1: ' + str(avg_f1))
