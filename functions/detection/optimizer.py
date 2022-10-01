6ee3739749fb74a9393885cea91bda8bd6443d19
SiqiZhou <541012232@qq.com>
2022-10-01 08:00:19 +0800

    111

--- functions/detection/optimizer.py
+++ functions/detection/optimizer.py
@@ -1,4 +1,4 @@
-from mealpy.bio_based import SMA, BBO
+from mealpy.bio_based import BBO
 from mealpy.evolutionary_based import GA
 from mealpy.music_based import HS
 from mealpy.swarm_based import PSO
@@ -7,15 +7,15 @@ from mealpy.system_based import AEO
 
 def get_optimizer(optimizer, problem_dict):
     if optimizer == 'BBO':
-        model = BBO.BaseBBO(problem_dict, epoch=100, pop_size=20)
+        model = BBO.BaseBBO(problem_dict, epoch=10, pop_size=50)
     elif optimizer == 'PSO':
-        model = PSO.BasePSO(problem_dict, epoch=100, pop_size=20)
+        model = PSO.BasePSO(problem_dict, epoch=10, pop_size=50)
     elif optimizer == 'AEO':
-        model = AEO.OriginalAEO(problem_dict, epoch=100, pop_size=20)
+        model = AEO.OriginalAEO(problem_dict, epoch=10, pop_size=50)
     elif optimizer == 'HS':
-        model = HS.BaseHS(problem_dict, epoch=100, pop_size=20)
+        model = HS.BaseHS(problem_dict, epoch=10, pop_size=50)
     elif optimizer == 'GA':
-        model = GA.BaseGA(problem_dict, epoch=100, pop_size=20)
+        model = GA.BaseGA(problem_dict, epoch=10, pop_size=50)
     else:
         return None
 
